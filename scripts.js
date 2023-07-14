d1e7ab0419ae2da7ab202de0bc94a64de3dedca2
Manonthemon <klimowicz.maciej@gmail.com>
2023-07-14 12:38:11 +0100

    Added cards, contact form, scrolling arrows, some basic styles and colors

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const scrollButtons = document.querySelectorAll('.scroll-button');
+  scrollButtons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+      if (index < pages.length - 1) {
+        currentPage = index + 1;
+        scrollToPage(currentPage);
+      } else {
+        currentPage = 0;
+        scrollToTop();
+      }
+    });
+  });
+});
+
 document.addEventListener('wheel', scrollHandler);
 
 let currentPage = 0;
@@ -22,6 +37,13 @@ function scrollHandler(e) {
 function scrollToPage(pageIndex) {
   window.scrollTo({
     top: pages[pageIndex].offsetTop,
+    behavior: 'smooth'
+  });
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
     behavior: 'smooth'
   });
 }
